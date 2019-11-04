32419c1dcd6014b10ba21061f88de49450d677a4
askec1705 <ahmetselimkaya1234@gmail.com>
2019-11-04 15:34:08 +0300

    Add project files.

---
+++ Odevım/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace Odevım
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstVeri1 = new System.Windows.Forms.ListBox();
+            this.lstVeri2 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lstVeri1
+            // 
+            this.lstVeri1.FormattingEnabled = true;
+            this.lstVeri1.Location = new System.Drawing.Point(12, 13);
+            this.lstVeri1.Name = "lstVeri1";
+            this.lstVeri1.Size = new System.Drawing.Size(256, 459);
+            this.lstVeri1.TabIndex = 0;
+            // 
+            // lstVeri2
+            // 
+            this.lstVeri2.FormattingEnabled = true;
+            this.lstVeri2.Location = new System.Drawing.Point(401, 13);
+            this.lstVeri2.Name = "lstVeri2";
+            this.lstVeri2.Size = new System.Drawing.Size(257, 459);
+            this.lstVeri2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.button1.Location = new System.Drawing.Point(274, 117);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 83);
+            this.button1.TabIndex = 2;
+            this.button1.Text = ">>";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.button2.Location = new System.Drawing.Point(274, 243);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(121, 83);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "<<";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(298, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "başla";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(670, 482);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lstVeri2);
+            this.Controls.Add(this.lstVeri1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstVeri1;
+        private System.Windows.Forms.ListBox lstVeri2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Odevım/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Odevım
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lstVeri1.SelectionMode = SelectionMode.MultiSimple;
+            lstVeri2.SelectionMode = SelectionMode.MultiSimple;
+        }
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            int[] veriler1 = new int[5];
+            veriler1[0] = 123456;
+            veriler1[1] = 23456;
+            veriler1[2] = 3456;
+            veriler1[3] = 456;
+            veriler1[4] = 56;
+            for (int i = 0; i <= veriler1.Length - 1; i++)
+            {
+                lstVeri1.Items.Add(veriler1[i]);
+            }
+            int[] veriler2 = new int[5];
+            veriler2[0] = 456789;
+            veriler2[1] = 56789;
+            veriler2[2] = 6789;
+            veriler2[3] = 789;
+            veriler2[4] = 89;
+            for (int i = 0; i <= veriler1.Length - 1; i++)
+            {
+                lstVeri2.Items.Add(veriler2[i]);
+            }
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            for (int i = lstVeri1.SelectedIndices.Count - 1; i >= 0; i--)
+            {
+                lstVeri2.Items.Add(lstVeri1.SelectedItems[i]);
+                lstVeri1.Items.RemoveAt(lstVeri1.SelectedIndices[i]);
+            }
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            for (int i = lstVeri2.SelectedIndices.Count - 1; i >= 0; i--)
+            {
+                lstVeri1.Items.Add(lstVeri2.SelectedItems[i]);
+                lstVeri2.Items.RemoveAt(lstVeri2.SelectedIndices[i]);
+            }
+        }
+    }
+}
